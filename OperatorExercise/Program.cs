f39f9988b197c4f8b094ed23560c187cb631625b
Stephen Madalo <stephenmadalo89@gmail.com>
2022-04-18 20:51:11 -0400

    initial commit

---
+++ OperatorExercise/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System;
+
+namespace OperatorExercise // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 17;
+            int b = 4;
+
+            int quotient = a / b;
+            int remainder = a % b;
+
+            if (a == 17 && b == 4) ;
+            Console.WriteLine($"Modulo of {a} and {b} is {quotient} with remainder of {remainder}");
+        }
+    }
+}
